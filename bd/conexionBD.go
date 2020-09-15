fed46319c51cf1e6832a4d2dff342961db21cc7a
david9393 <deividbest9393@gmail.com>
2020-09-14 22:06:20 -0500

    heroku

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -9,19 +9,27 @@ import (
 )
 
 const (
-	host     = "localhost"
+	// host     = "localhost"
+	// port     = 5432
+	// user     = "postgres"
+	// password = "admin"
+	// dbname   = "groomerapp"
+	host     = "ec2-54-243-67-199.compute-1.amazonaws.com"
 	port     = 5432
-	user     = "postgres"
-	password = "admin"
-	dbname   = "groomerapp"
+	user     = "qtrvvsjxosvyjl"
+	password = "3e861e1966590ebed9b267be2f18ed2c49ed59155426374e7945576441cb4d1e"
+	dbname   = "dcn8aj88hcvj73"
 )
 
 var PostgresCN = ConectarBD()
 
 func ConectarBD() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+		"password=%s dbname=%s",
 		host, port, user, password, dbname)
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	// "password=%s dbname=%s sslmode=disable",
+	// host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Printf(err.Error())
